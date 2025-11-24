ccb0e216b80aa98aeb39ccac5e8a30f4df594094
nxdinh94 <nguyenxuandinh336@gmail.com>
2025-11-24 19:28:00 +0700

    feat: Add NotificationScreen and NotificationItem widget for notifications display

---
+++ lib/features/notification/presentations/screens/notification_screen.dart
@@ -0,0 +1,320 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:kit/features/home/presentation/pages/for_you_tab.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+class NotificationScreen extends StatefulWidget {
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  bool verticalGallery = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExampleAppBarLayout(
+      title: "Gallery Example",
+      showGoBack: true,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GalleryExampleItemThumbnail(
+                    galleryExampleItem: galleryItems[0],
+                    onTap: () {
+                      open(context, 0);
+                    },
+                  ),
+                  GalleryExampleItemThumbnail(
+                    galleryExampleItem: galleryItems[2],
+                    onTap: () {
+                      open(context, 2);
+                    },
+                  ),
+                  GalleryExampleItemThumbnail(
+                    galleryExampleItem: galleryItems[3],
+                    onTap: () {
+                      open(context, 3);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text("Vertical"),
+                Checkbox(
+                  value: verticalGallery,
+                  onChanged: (value) {
+                    setState(() {
+                      verticalGallery = value!;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void open(BuildContext context, final int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GalleryPhotoViewWrapper(
+          galleryItems: galleryItems,
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          initialIndex: index,
+          scrollDirection: verticalGallery ? Axis.vertical : Axis.horizontal,
+        ),
+      ),
+    );
+  }
+}
+
+class GalleryPhotoViewWrapper extends StatefulWidget {
+  GalleryPhotoViewWrapper({
+    this.loadingBuilder,
+    this.backgroundDecoration,
+    this.minScale,
+    this.maxScale,
+    this.initialIndex = 0,
+    required this.galleryItems,
+    this.scrollDirection = Axis.horizontal,
+  }) : pageController = PageController(initialPage: initialIndex);
+
+  final LoadingBuilder? loadingBuilder;
+  final BoxDecoration? backgroundDecoration;
+  final dynamic minScale;
+  final dynamic maxScale;
+  final int initialIndex;
+  final PageController pageController;
+  final List<GalleryExampleItem> galleryItems;
+  final Axis scrollDirection;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GalleryPhotoViewWrapperState();
+  }
+}
+
+class _GalleryPhotoViewWrapperState extends State<GalleryPhotoViewWrapper> {
+  late int currentIndex = widget.initialIndex;
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: widget.backgroundDecoration,
+        constraints: BoxConstraints.expand(
+          height: MediaQuery.of(context).size.height,
+        ),
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            PhotoViewGallery.builder(
+              scrollPhysics: const BouncingScrollPhysics(),
+              builder: _buildItem,
+              itemCount: widget.galleryItems.length,
+              loadingBuilder: widget.loadingBuilder,
+              backgroundDecoration: widget.backgroundDecoration,
+              pageController: widget.pageController,
+              onPageChanged: onPageChanged,
+              scrollDirection: widget.scrollDirection,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Image ${currentIndex + 1}",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  decoration: null,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  PhotoViewGalleryPageOptions _buildItem(BuildContext context, int index) {
+    final GalleryExampleItem item = widget.galleryItems[index];
+    return item.isSvg
+        ? PhotoViewGalleryPageOptions.customChild(
+            child: Container(
+              width: 300,
+              height: 300,
+              child: SvgPicture.network(
+                item.resource,
+                height: 200.0,
+              ),
+            ),
+            childSize: const Size(300, 300),
+            initialScale: PhotoViewComputedScale.contained,
+            minScale: PhotoViewComputedScale.contained * (0.5 + index / 10),
+            maxScale: PhotoViewComputedScale.covered * 4.1,
+            heroAttributes: PhotoViewHeroAttributes(tag: item.id),
+          )
+        : PhotoViewGalleryPageOptions(
+            imageProvider: Image.network(item.resource).image,
+            initialScale: PhotoViewComputedScale.contained,
+            minScale: PhotoViewComputedScale.contained * (0.5 + index / 10),
+            maxScale: PhotoViewComputedScale.covered * 4.1,
+            heroAttributes: PhotoViewHeroAttributes(tag: item.id),
+          );
+  }
+}
+
+
+
+class GalleryExampleItemThumbnail extends StatelessWidget {
+  const GalleryExampleItemThumbnail({
+    Key? key,
+    required this.galleryExampleItem,
+    required this.onTap,
+  }) : super(key: key);
+
+  final GalleryExampleItem galleryExampleItem;
+
+  final GestureTapCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Hero(
+          tag: galleryExampleItem.id,
+          child: Image.network(galleryExampleItem.resource, height: 80.0),
+        ),
+      ),
+    );
+  }
+}
+
+List<GalleryExampleItem> galleryItems = <GalleryExampleItem>[
+  GalleryExampleItem(
+    id: "tag1",
+    resource: "https://avatarmoi.com/wp-content/uploads/2025/07/Anh-gai-xinh-2k5-deo-kinh-can-dang-yeu.webp",
+  ),
+  GalleryExampleItem(
+    id: "tag2", 
+    resource: "https://auvi.edu.vn/wp-content/uploads/2025/02/anh-gai-xinh-trung-quoc-2.jpg", 
+    isSvg: false
+  ),
+  GalleryExampleItem(
+    id: "tag3",
+    resource: "https://macshop24h.com/wp-content/uploads/2025/07/anh-gai-xinh-trung-quoc-20.jpeg",
+  ),
+  GalleryExampleItem(
+    id: "tag4",
+    resource: "https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg",
+  ),
+];
+
+
+class ExampleAppBar extends StatelessWidget {
+  const ExampleAppBar({required this.title, this.showGoBack = false}) : super();
+
+  final String title;
+  final bool showGoBack;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.only(
+              bottomLeft: const Radius.circular(10.0),
+              bottomRight: const Radius.circular(10.0),
+            ),
+            boxShadow: <BoxShadow>[
+              const BoxShadow(
+                  color: Colors.black12, spreadRadius: 10.0, blurRadius: 20.0)
+            ]),
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: showGoBack
+                  ? IconButton(
+                      icon: const Icon(Icons.chevron_left),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      padding: EdgeInsets.zero,
+                    )
+                  : Container(
+                      height: 50.0,
+                    ),
+            ),
+            Expanded(
+              child: Text(
+                title,
+                style: const TextStyle(
+                    fontSize: 25.0, fontWeight: FontWeight.w700),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExampleAppBarLayout extends StatelessWidget {
+  const ExampleAppBarLayout({
+    Key? key,
+    required this.title,
+    this.showGoBack = false,
+    required this.child,
+  }) : super(key: key);
+
+  final String title;
+  final bool showGoBack;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ExampleAppBar(
+              title: title,
+              showGoBack: showGoBack,
+            ),
+            Expanded(
+              child: child,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
